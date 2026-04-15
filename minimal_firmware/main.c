23e569955df03535605b5d64eeee30ce11cc84aa
DilonH <dilonhewamanna@outlook.com>
2026-04-15 23:06:19 +1000

    minimal firmware

---
+++ minimal_firmware/main.c
@@ -0,0 +1,37 @@
+/*
+Author: Dilon Hewamanna
+Date: 31/02/26
+
+main file for blinking an LED on flight controller
+
+TODO:
+  Double check that H7 has 150 additional peripheral handlers
+  Learn how to properly define memory sections in linker file
+*/
+
+
+int main(void){
+  return 0;   // do nothing for now
+}
+
+
+///////              RESET HANDLER (MAKE SURE TO CHECK JUST THIS ON BOARD)
+// Startup code
+__attribute__((naked, noreturn)) void _reset(void) {
+  // memset .bss to zero, and copy .data section to RAM region
+  extern long _sbss, _ebss, _sdata, _edata, _sidata;
+  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+  main();             // Call main()
+  for (;;) (void) 0;  // Infinite loop in the case if main() returns
+}
+
+extern void _estack(void);  // Defined in link.ld
+
+// 16 standard and 150 STM32-specific handlers
+__attribute__((section(".vectors"))) void (*const tab[16 + 150])(void) = {
+  _estack, _reset
+};
+/////// 
+
